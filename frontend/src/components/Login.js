8d5a81cb0a7ef76214dc64c8782dec1bcb96c56a
Cathy Do <catherinexdo@gmail.com>
2023-08-15 08:27:55 -0700

    new login page

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,60 @@
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import axiosInstance from "../axios";
+
+export default function Login() {
+    const [loginForm, setLoginForm] = useState({
+        email: '',
+        password: ''
+    });
+    const navigate = useNavigate();
+    function handleChange(event) {
+        setLoginForm({
+            ...loginForm,
+            [event.target.name]:  event.target.value
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        axiosInstance.post(`token/`, loginForm)
+		.then((result) => {
+				localStorage.setItem('access_token', result.data.access);
+				localStorage.setItem('refresh_token', result.data.refresh);
+                localStorage.setItem('user',JSON.stringify(result.data));
+				axiosInstance.defaults.headers['Authorization'] =
+					'JWT ' + localStorage.getItem('access_token');
+                console.log(result);
+                navigate('/');
+			});
+    }
+    return (
+
+        <div className="LoginForm">
+            <form action="" class="form">
+                <h1 class="title">Login</h1>
+    
+                <div className="inputContainer">
+                    <label htmlFor="email" className="label">Email</label>
+                    <input type="text" className="input" name="email" value={loginForm.email} onChange={handleChange}/>
+                </div>
+    
+    
+                <div className="inputContainer">
+                    <label htmlFor="password" className="label">Password</label>
+                    <input type="password" className="input" name="password" value={loginForm.password} onChange={handleChange} /> 
+                </div>
+    
+
+                <input type="submit" className="primary-button" value="Login" onClick={handleSubmit} />
+                <div>
+                    <a href="">Forgot password?</a>
+                </div>
+                <div>
+                    <Link to="/signup">Sign Up</Link>
+                </div>
+            </form>
+        </div>
+
+    )
+}
