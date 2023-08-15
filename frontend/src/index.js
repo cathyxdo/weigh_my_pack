d743a67089baaa4014e2d9ac91f5e8ce5fd73d71
Cathy Do <catherinexdo@gmail.com>
2023-08-15 08:24:37 -0700

    added react router broswer router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import './simple.css';
 import './index.css';
-
+import Login from './components/Login';
 import App from './App';
+import PageNotFound from './components/PageNotFound';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from './components/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+/* root.render(
   <React.StrictMode>
     <App /> 
+    <Login />
+  </React.StrictMode>
+); */
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
