face5b8f9509efd43eba955c7b6651ec72d1a39a
Cathy Do <catherinexdo@gmail.com>
2023-08-15 17:03:24 -0700

    added logout functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,25 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../axios";
 
 export default function Header() {
+    const navigate = useNavigate();
+    function logout() {
+        console.log('logout');
+        const response = axiosInstance.post('user/logout/blacklist/', {
+			refresh_token: localStorage.getItem('refresh_token'),
+		});
+		localStorage.removeItem('access_token');
+		localStorage.removeItem('refresh_token');
+		axiosInstance.defaults.headers['Authorization'] = null;
+        navigate('/login');
+
+    }
     return (
         <>
             <Link to="/signup"><button className="secondary-button">Sign Up</button></Link>
-            <Link to="/login"><button className="primary-button">Log In</button></Link>
+            <Link to="/login"><button className="primary-button">Login</button></Link>
+            <button className="primary-button" onClick={logout}>Logout</button>
         </>
     )
 }
