c5b3ffd96bc4538237bb071dfe2c850b67783eec
Cathy Do <catherinexdo@gmail.com>
2023-08-17 11:10:58 -0700

    added changepassword path

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CustomUserCreate, BlacklistTokenUpdateView
+from .views import CustomUserCreate, BlacklistTokenUpdateView, ChangePasswordView
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', CustomUserCreate.as_view(), name='create_user'),
     path('logout/blacklist/', BlacklistTokenUpdateView.as_view(), name='blacklist'),
+    path('changepassword/', ChangePasswordView.as_view(), name="change_password"),
 ]
