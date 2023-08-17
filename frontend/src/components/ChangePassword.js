318f35661ea48c384c160388df0adcf6e6fe6b2f
Cathy Do <catherinexdo@gmail.com>
2023-08-17 11:11:57 -0700

    new page to change password

---
+++ frontend/src/components/ChangePassword.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import axiosInstance from "../axios";
+import { useNavigate } from "react-router-dom";
+
+export default function ChangePassword() {
+    const [passwords, setPasswords] = useState({
+        old_password: '',
+        password: ''
+    })
+    const navigate = useNavigate()
+    function handleChange(event) {
+        setPasswords({
+            ...passwords,
+            [event.target.name]: event.target.value
+        })
+    }
+    function handleSubmit(event) {
+        event.preventDefault()
+        axiosInstance.put(`user/changepassword/`, passwords)
+		.then((result) => {
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('refresh_token');
+            axiosInstance.defaults.headers['Authorization'] = null;
+            navigate('/login');
+		});
+    }
+
+    return (
+        <div className="ChangePasswordForm">
+            <form action="" className="form">
+                <h1 class="title">Login</h1>
+    
+                <div className="inputContainer">
+                    <label htmlFor="old_password" className="label">Old Password</label>
+                    <input type="password" className="input" name="old_password" value={passwords.old_password} onChange={handleChange}/>
+                </div>
+    
+    
+                <div className="inputContainer">
+                    <label htmlFor="password" className="label">New Password</label>
+                    <input type="password" className="input" name="password" value={passwords.password} onChange={handleChange} /> 
+                </div>
+    
+
+                <input type="submit" className="primary-button" value="Change Password" onClick={handleSubmit} />
+
+
+            </form>
+        </div>
+    )
+}
