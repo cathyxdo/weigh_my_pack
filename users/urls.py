8f71d7496dd9d3a0e9e51821308bc1539217d981
Cathy Do <catherinexdo@gmail.com>
2023-10-12 13:27:36 -0700

    adding forgot password links

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import CustomUserCreate, BlacklistTokenUpdateView, ChangePasswordView
+from .views import CustomUserCreate, BlacklistTokenUpdateView, ChangePasswordView, PasswordResetRequestView, PasswordResetConfirmView
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', CustomUserCreate.as_view(), name='create_user'),
     path('logout/blacklist/', BlacklistTokenUpdateView.as_view(), name='blacklist'),
-    path('changepassword/', ChangePasswordView.as_view(), name="change_password"),
+    path('changepassword/', ChangePasswordView.as_view(), name='change_password'),
+    path('forgot-password/', PasswordResetRequestView.as_view(), name='forgot_password'),
+    path('reset-password/<str:uidb64>/<str:token>/', PasswordResetConfirmView.as_view(), name="reset_password"),
 ]
