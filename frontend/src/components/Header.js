f1fca64d8f51529d3ec969687ae41b4e7be35405
Cathy Do <catherinexdo@gmail.com>
2023-12-02 11:51:24 -0800

    Updated logic to show different user log in options depending if a user is logged in or out

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../axios";
 
-export default function Header() {
+export default function Header({ isLoggedIn }) {
     const navigate = useNavigate();
     function logout(event) {
         console.log('logout');
@@ -16,11 +16,20 @@ export default function Header() {
 
     }
     return (
-        <>
-            <Link to="/signup"><button className="secondary-button">Sign Up</button></Link>
-            <Link to="/login"><button className="primary-button">Login</button></Link>
-            <Link to="/changepassword">Change Password</Link>
-            <button className="primary-button" onClick={logout}>Logout</button>
-        </>
+        <div className="userHeader">
+            {!isLoggedIn && 
+                <>
+                    <Link to="/signup"><button className="secondary-button">Sign Up</button></Link>
+                    <Link to="/login"><button className="primary-button">Login</button></Link>
+                </>
+            }
+            {isLoggedIn && 
+                <>
+                    <button className="primary-button" onClick={logout}>Logout</button>
+                    <Link to="/changepassword">Change Password</Link>
+
+                </>
+            }
+        </div>
     )
 }
