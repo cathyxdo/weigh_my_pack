f5bc63a580b956bb3e6b8701060ba924b11a0cb0
Cathy Do <catherinexdo@gmail.com>
2024-12-23 19:29:55 -0800

    only output chart if data is available

--- frontend/src/components/ChartSection.js
+++ frontend/src/components/ChartSection.js
@@ -112,8 +112,11 @@ export default function ChartSection({list}) {
                 </table>
                 
                 <div className="donut-chart">
+                {data.length > 0 && ( 
                     <DoughnutChart chartData={chartData} />
+                )}
                 </div>
+
             </div>
     )
 }
