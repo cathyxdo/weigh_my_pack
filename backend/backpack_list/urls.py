a8b872ba380958a3b29f63bcfffcb9750cfb0eee
Cathy Do <catherinexdo@gmail.com>
2025-01-04 09:12:35 -0800

    change pk type to str to account for uuid

--- backend/backpack_list/urls.py
+++ backend/backpack_list/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import (
     ListList,
     ModifyCategory,
@@ -14,7 +14,7 @@ app_name = 'backpack_list'
 urlpatterns = [
     path('lists/', ListList.as_view(), name='lists'),
     path('users/lists/', UserListView.as_view(), name='user_lists'),
-    path('lists/<int:pk>/', SingleList.as_view(), name='list_modify'),
+    path('lists/<str:pk>/', SingleList.as_view(), name='list_modify'),
     path('categories/<int:pk>/', ModifyCategory.as_view(), name='category_modify'),
     path('categories/', CreateCategory.as_view(), name='create_category'),
     path('items/<int:pk>/', ModifyItem.as_view(), name='item_modify'),
