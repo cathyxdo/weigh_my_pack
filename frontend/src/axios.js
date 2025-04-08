5ba86882a495d3d7c1ae23b8b0aa90439a854afb
Cathy Do <catherinexdo@gmail.com>
2025-04-08 14:48:47 -0700

    correct login url to include #

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -36,7 +36,7 @@ axiosInstance.interceptors.response.use(
       error.response.status === 401 &&
       originalRequest.url === baseURL + "user/token/refresh/"
     ) {
-      window.location.href = "/login/";
+      window.location.href = "/#/login/";
       return Promise.reject(error);
     }
 
@@ -72,11 +72,11 @@ axiosInstance.interceptors.response.use(
             });
         } else {
           console.log("Refresh token is expired", tokenParts.exp, now);
-          window.location.href = "/login/";
+          window.location.href = "/#/login/";
         }
       } else {
         console.log("Refresh token not available.");
-        window.location.href = "/login/";
+        window.location.href = "/#/login/";
       }
     }
 
